7454bc21ea3689ed4e19fc791a4cc99998831fef
mahfujseopage1 <mahfujar@seopage1.net>
2024-11-23 14:04:35 +0600

    testing

--- app/api/route.js
+++ app/api/route.js
@@ -19,10 +19,7 @@ async function getBrowser() {
 
     const browser = await puppeteerCore.launch({
       args: chromium.args,
-      defaultViewport: {
-        width: 1200, // Default values
-        height: 800, // Default values
-      },
+      defaultViewport: chromium.defaultViewport,
       executablePath,
       headless: chromium.headless,
     });
@@ -35,50 +32,16 @@ async function getBrowser() {
   }
 }
 
-export async function POST(request) {
-  // Parse the incoming JSON body
-  const body = await request.json();
+export async function GET() {
+  const browser = await getBrowser();
 
-  const { url, width, height } = body; // Destructure URL, width, and height from request body
-
-  // Ensure default values are set if not provided in the request
-  const pageWidth = width || 1200;
-  const pageHeight = height || 800;
-
-  try {
-    const browser = await getBrowser();
-
-    const page = await browser.newPage();
-    await page.goto(url, { waitUntil: 'networkidle2' });
-
-    // Handle scrolling on the page
-    await page.evaluate(() => {
-      let totalHeight = 0;
-      const distance = 100;
-      const scrollHeight = document.body.scrollHeight;
-
-      const scroll = () => {
-        window.scrollBy(0, distance);
-        totalHeight += distance;
-
-        if (totalHeight < scrollHeight) {
-          setTimeout(scroll, 100);
-        }
-      };
-
-      scroll();
-    });
-
-    // Capture the screenshot as a base64 string
-    const screenshotBase64 = await page.screenshot({ fullPage: true, encoding: 'base64' });
-
-    console.log('Screenshot taken', screenshotBase64.length);
-
-    await browser.close();
-
-    return new NextResponse(screenshotBase64);
-  } catch (error) {
-    console.error('Error taking screenshot:', error);
-    return new NextResponse('Failed to take screenshot', { status: 500 });
-  }
+  const page = await browser.newPage();
+  await page.goto("https://example.com");
+  const pdf = await page.pdf();
+  await browser.close();
+  return new NextResponse(pdf, {
+    headers: {
+      "Content-Type": "application/pdf",
+    },
+  });
 }
